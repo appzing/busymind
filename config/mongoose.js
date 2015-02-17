59b340d9d0d59833f8e59dca59110735a40bf260
urmila <urmilapradeep@gmail.com>
2015-02-17 12:45:11 -0800

    Added databse credentials

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,8 +8,9 @@ var connect = function() {
 	//specify options when mongoose connects to mongodb
 	var options = { server: { socketOptons: {keepAlive: 1}}}
 	//connect to our mongodb database server with options specified above
-	mongoose.connect('mongodb://busymind.herokuapp.com/boardApp', options)
-	//mongoose.connect('mongodb://localhost/boardApp', options)
+	//mongoose.connect('mongodb://busymind.herokuapp.com/boardApp', options)
+	mongoose.connect('mongodb://heroku_app33914762:u3fiujrmlksiirg0h8jrtpts0e@ds045031.mongolab.com:45031/heroku_app33914762', options);
+	//mongoose.connect('mongodb://localhost/boardApp', options);
 }
 //actually connect to the database "DONT FORGET THIS"
 connect();
